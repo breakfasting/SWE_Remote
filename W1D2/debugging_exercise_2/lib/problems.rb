06ab9d1ba0baa609563b64bdecb7716920ad3f48
breakfasting <breakfast.ing@gmail.com>
2020-02-12 00:26:40 -0800

    debugging exercise 2 done

--- W1D2/debugging_exercise_2/lib/problems.rb
+++ W1D2/debugging_exercise_2/lib/problems.rb
@@ -4,20 +4,20 @@
 require "byebug"
 
 def largest_prime_factor(num)
-    # debugger
-    # (num..2).each do |idx|
-    #     if num % idx == 0 && prime?(idx)
-    #         return idx
-    #     end
+  # debugger
+  # (num..2).each do |idx|
+  #     if num % idx == 0 && prime?(idx)
+  #         return idx
+  #     end
 
-    # end
-    i = num
-    while i >= 2
-        if num % i == 0 && prime?(i)
-             return i
-         end
-    i -= 1
+  # end
+  i = num
+  while i >= 2
+    if num % i == 0 && prime?(i)
+      return i
     end
+    i -= 1
+  end
 end
 
 def prime?(num)
@@ -29,4 +29,54 @@ def prime?(num)
     end
   end
   return true
+end
+
+def unique_chars?(str) #hello
+  #debugger
+  word_count = Hash.new(0)
+  str.each_char do |char|
+    if word_count[char] != 0
+      return false
+    else
+      word_count[char] += 1
+    end
+  end
+  true
+end
+
+p unique_chars?("computer")
+
+def dupe_indices(array) # ["a", "b", "c", "c", "b"] -> { "b" => [1, 4], "c" => [2, 3] }
+  dupe = {}
+  array.each_with_index do |ele, i|
+    if !dupe[ele].nil?
+      dupe[ele] << i
+    else
+      dupe[ele] = [i]
+    end
+  end
+
+  dupe.reject { |k, v| v.length < 2 }
+  # dupe = Hash.new{Array.new()}
+  # array.each_with_index do |ele , i|
+  #      if dupe[ele] != nil
+  #         dupe[ele] << i
+  #      else
+  #       dupe[ele] << i
+
+  #     end
+  # end
+  # return dupe
+end
+
+def ana_array(arr1, arr2)
+  hash1 = Hash.new(0)
+  hash2 = Hash.new(0)
+  arr1.each do |ele|
+    hash1[ele] += 1
+  end
+  arr2.each do |ele|
+    hash2[ele] += 1
+  end
+  hash1 == hash2
 end
