d26264bd1043ba6ed2157512d5d448ca21c0329a
breakfasting <breakfast.ing@gmail.com>
2020-02-23 19:12:29 -0800

    finished battleship timed in 25m44s

--- W2Assessment/battleship_project/lib/battleship.rb
+++ W2Assessment/battleship_project/lib/battleship.rb
@@ -3,4 +3,52 @@ require_relative "player"
 
 class Battleship
 
+  attr_reader :board, :player
+
+  def initialize(n)
+    @player = Player.new
+    @board = Board.new(n)
+    @remaining_misses = @board.size / 2
+  end
+  
+  def start_game
+    @board.place_random_ships
+    puts @board.size / 4
+    @board.print
+  end
+
+  def lose?
+    if @remaining_misses <= 0
+      puts 'you lose'
+      return true
+    else
+      return false
+    end
+  end
+
+  def win?
+    if @board.num_ships == 0
+      puts 'you win'
+      return true
+    else
+      return false
+    end
+  end
+
+  def game_over?
+    self.win? || self.lose?
+  end
+
+  def turn
+    resp_arr = @player.get_move
+    if @board.attack(resp_arr)
+      @board.print
+    else
+      @remaining_misses -= 1
+      @board.print
+    end
+    puts @remaining_misses
+  end
 end
+
+# Done in 25m44s
--- W2Assessment/battleship_project/lib/board.rb
+++ W2Assessment/battleship_project/lib/board.rb
@@ -1,3 +1,67 @@
 class Board
-  
+
+  attr_reader :size
+
+  def initialize(n)
+    @grid = Array.new(n) { Array.new(n, :N) }
+    @size = n * n
+  end
+
+  def [](pos_arr)
+    @grid[pos_arr[0]][pos_arr[1]]
+  end
+
+  def []=(pos_arr, val)
+    @grid[pos_arr[0]][pos_arr[1]] = val
+  end
+
+  def num_ships
+    @grid.sum do |row|
+      row.count { |ele| ele == :S }
+    end
+  end
+
+  # Part 2
+  def attack(pos_arr)
+    if self[pos_arr] == :S
+      self[pos_arr] = :H
+      puts 'you sunk my battleship!'
+      return true
+    else
+      self[pos_arr] = :X
+      return false
+    end
+  end
+
+  def place_random_ships
+    amt_ships = @size / 4
+    while amt_ships > 0
+      row = rand(@grid.length - 1)
+      col = rand(@grid.length - 1)
+      if self[[row, col]] == :N
+        self[[row, col]] = :S
+        amt_ships -= 1
+      end
+    end
+  end
+
+  def hidden_ships_grid
+    hidden_grid = @grid.map do |row|
+      row.map { |ele| ele == :S ? :N : ele }
+    end
+  end
+
+  def self.print_grid(grid)
+    grid.each do |row|
+      puts row.join(" ")
+    end
+  end
+
+  def cheat
+    Board.print_grid(@grid)
+  end
+
+  def print
+    Board.print_grid(hidden_ships_grid)
+  end
 end
