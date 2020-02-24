0fb0c09b2417ac0e155596dbb6278879a50c7725
breakfasting <breakfast.ing@gmail.com>
2020-02-23 19:32:10 -0800

    finished mastermind timed in 15m44s

--- W2Assessment/mastermind_project/lib/code.rb
+++ W2Assessment/mastermind_project/lib/code.rb
@@ -6,4 +6,58 @@ class Code
     "Y" => :yellow
   }
 
+  attr_reader :pegs
+
+  def self.valid_pegs?(char_arr)
+    char_arr.all? { |char| POSSIBLE_PEGS.include?(char.upcase) }
+  end
+
+  def initialize(char_arr)
+    if !Code.valid_pegs?(char_arr)
+      raise "ERROR: Not valid pegs"
+    else
+      @pegs = char_arr.map(&:upcase)
+    end
+  end
+  
+  def self.random(n)
+    gen_char_arr = Array.new(n) { POSSIBLE_PEGS.keys.sample }
+    Code.new(gen_char_arr)
+  end
+
+  def self.from_string(str)
+    str_arr = str.split('')
+    Code.new(str_arr)
+  end
+
+  def [](idx)
+    @pegs[idx]
+  end
+
+  def length
+    @pegs.length
+  end
+
+  # Part 2
+  def num_exact_matches(guess)
+    count = 0
+    guess.pegs.each_with_index do |peg, i|
+      count += 1 if peg == @pegs[i]
+    end
+    count
+  end
+
+  def num_near_matches(guess)
+    count = 0
+    guess.pegs.each_with_index do |peg, i|
+      if peg != @pegs[i] && @pegs.include?(peg)
+        count += 1
+      end
+    end
+    count
+  end
+
+  def ==(code)
+    self.pegs == code.pegs
+  end
 end
--- W2Assessment/mastermind_project/lib/mastermind.rb
+++ W2Assessment/mastermind_project/lib/mastermind.rb
@@ -1,5 +1,21 @@
 require_relative "code"
 
 class Mastermind
+  def initialize(n)
+    @secret_code = Code.random(n)
+  end
 
+  def print_matches(guess_code)
+    puts @secret_code.num_exact_matches(guess_code)
+    puts @secret_code.num_near_matches(guess_code)
+  end
+
+  def ask_user_for_guess
+    puts 'Enter a code'
+    guess = Code.from_string(gets.chomp)
+    print_matches(guess)
+    return @secret_code == guess
+  end
 end
+
+# Done in 15m44s
